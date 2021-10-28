1a6e214caa36e04ee8a0ec90fd28d154df0375ef
kuris-c <93099661+kuris-c@users.noreply.github.com>
2021-10-28 09:40:01 +0100

    Update prepro.h

--- HeaderFiles/prepro.h
+++ HeaderFiles/prepro.h
@@ -3,8 +3,17 @@
 #include <string>
 #include <Windows.h>
 
+//Global Variables + Settings Functions
+extern std::string playerLocation; //Stores Player Location
+void gameLogo(); //Display Game Logo + Centered
+void displayGameWindow(int width, int height); //Centre and Size game Window
+void textTyper(std::string inputText); //Type Text In Typewriter Style
+void inputTooltip(); //Allows the player to know when to input text
+void gamePlay(); //Main script to handle all gameplay elements
+void createPrison();
+
 //Initialize the player
-class PlayerStats
+class Player
 {
 public:
 	//Player Inputted Details
@@ -22,19 +31,106 @@ public:
 	int playerSprinting;
 };
 
-//Other Main Characters
-extern PlayerStats playerSidekick;
-
-//Global Variables
-
-//Text Progressive Typer
-void textTyper(std::string inputText)
+//Initialize side characters class
+class SideCharacters
 {
-	int i = 0;
-	while (inputText[i] != '\0')
+public:
+	//Player Inputted Details
+	std::string playerName;
+	int playerAge;
+
+	//Player Ingame Stats
+	int playerHitPoints;
+	int playerHunger;
+
+	//Player Skills
+	int playerLockpicking;
+	int playerTunneling;
+	int playerDeceit;
+	int playerSprinting;
+};
+
+//Extern to Side Characters
+extern SideCharacters playerSidekick;
+
+//Initialize the rooms
+class PrisonRooms
+{
+public:
+	bool playerAccess;						//If Player Can Access The Room
+	bool firstVisit;						//If Player Is Visiting For The First Time
+	std::string roomDesc1;					//If Player Wakes In The Room
+	std::string roomDesc2;					//If Player Walks Into The Room
+	std::string roomDescription;			//Description Of The Room And What Can Be Seen
+	//int pPresentAtDay[14];					//Prisoners Present At Each Hour Of The Day
+	//int pPresentAtNight;					//Prisoners Present At Night
+	int gPresentAtDay;						//Guards Present During The Day
+	int gPresentAtNight;					//Guards Present During The Night
+	bool playerActions[7];					//How Many Actions The Player Can Choose In Each Room
+	std::string actionDescription[7];		//Description Of The Actions
+	bool locationsOpen[18];					//How Many Rooms Are Accessible
+	std::string locationsDescription[18];	//Description Of The Rooms
+
+
+	void showRoomDescription()
 	{
-		std::cout << inputText[i];
-		Sleep(18);
-		i++;
-	};
-}
+		system("CLS");
+		if (firstVisit)
+		{
+			textTyper(roomDesc2 + roomDescription);
+			firstVisit = false;
+		}
+
+		else
+		{
+			std::cout << roomDescription;
+		}
+	}
+
+	void showRoomActions()
+	{
+		std::cout << "\n\n";
+
+		for (int i = 0; i != 7; i++)
+		{
+			if (playerActions[i] == true)
+			{
+				std::cout << actionDescription[i];
+			}
+		}
+	}
+
+	void showRoomLocations()
+	{
+		std::cout << "\n";
+
+		for (int i = 0; i != 18; i++)
+		{
+			if (locationsOpen[i] == true)
+			{
+				std::cout << locationsDescription[i];
+			}
+		}
+	}
+};
+
+//Extern to Prison Rooms
+extern PrisonRooms cellBlockA;
+extern PrisonRooms cellBlockB;
+extern PrisonRooms cellBlockC;
+extern PrisonRooms cellBlockD;
+extern PrisonRooms reception;
+extern PrisonRooms courtyard;
+extern PrisonRooms canteen;
+extern PrisonRooms kitchen;
+extern PrisonRooms infirmary;
+extern PrisonRooms chapel;
+extern PrisonRooms commonRoom;
+extern PrisonRooms laundry;
+extern PrisonRooms classroom;
+extern PrisonRooms prisonShop;
+extern PrisonRooms visitation;
+extern PrisonRooms personalCell;
+extern PrisonRooms tunnel;
+extern PrisonRooms therapistOffice;
+extern PrisonRooms shower;
